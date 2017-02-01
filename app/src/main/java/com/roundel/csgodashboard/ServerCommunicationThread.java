ee852867bdf1bcdd81bab6770759e3748d3e08b0
RouNdeL <krzys.zdulski@gmail.com>
2017-02-01 16:40:32 +0100

    Cleaned up server connection process (added port and device name)

--- app/src/main/java/com/roundel/csgodashboard/ServerCommunicationThread.java
+++ app/src/main/java/com/roundel/csgodashboard/ServerCommunicationThread.java
@@ -1,6 +1,11 @@
 package com.roundel.csgodashboard;
 
+import android.os.Build;
+
 import com.roundel.csgodashboard.entities.GameServer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -8,53 +13,89 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Objects;
 
 /**
  * Created by Krzysiek on 2017-01-31.
  */
 public class ServerCommunicationThread extends Thread implements Runnable
 {
+    public static final int MODE_CONNECT = 0;
+
     private static final String TAG = ServerCommunicationThread.class.getSimpleName();
+
+    private static final String CONNECTION_REQUEST = "CSGO_DASHBOARD_CONNECTION_REQUEST";
+    private static final String CONNECTION_RESPONSE = "CSGO_DASHBOARD_CONNECTION_RESPONSE";
+    private static final String CONNECTION_GRANTED = "CSGO_DASHBOARD_ACCESS_GRANTED";
+    private static final String CONNECTION_DENIED = "CSGO_DASHBOARD_ACCESS_DENIED";
 
     private GameServer gameServer;
     private Socket gameServerSocket;
-    private String data;
-    private ServerCommunication listener;
+    private int communicationMode;
 
-    public ServerCommunicationThread(GameServer gameServer, String data)
+    private ServerConnectionListener connectionListener;
+
+    private int gameListeningPort = -1;
+
+    public ServerCommunicationThread(GameServer gameServer, int communicationMode, int... args)
     {
         this.gameServer = gameServer;
-        this.data = data;
-    }
-
-    public void setServerCommunication(ServerCommunication listener)
-    {
-        this.listener = listener;
+        this.communicationMode = communicationMode;
+        switch(communicationMode)
+        {
+            case MODE_CONNECT:
+                if(args.length < 1)
+                    throw new IllegalArgumentException("You have to supply a port that the app is going to send game info to, when using MODE_CONNECT.");
+                gameListeningPort = args[0];
+                break;
+            default:
+                throw new IllegalArgumentException("Mode has to be one of [MODE_CONNECT]");
+        }
     }
 
     @Override
     public void run()
     {
-        try
+        switch(communicationMode)
         {
-            gameServerSocket = new Socket(gameServer.getHost(), gameServer.getPort());
-            sendBytes(data.getBytes());
-            byte[] response = receiveBytes();
-            if(listener != null)
-                listener.onReceive(response);
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
+            case MODE_CONNECT:
+            {
+                try
+                {
+                    gameServerSocket = new Socket(gameServer.getHost(), gameServer.getPort());
+                    sendBytes(gameServerSocket, CONNECTION_REQUEST.getBytes());
+                    byte[] response = receiveBytes();
+                    if(Objects.equals(new String(response).trim(), CONNECTION_RESPONSE))
+                    {
+                        JSONObject json = new JSONObject();
+                        json.put("device_name", Build.MANUFACTURER + " " + Build.MODEL);
+                        json.put("game_port", gameListeningPort);
+                        sendBytes(gameServerSocket, json.toString().getBytes());
+                        response = receiveBytes();
+                        if(Objects.equals(new String(response).trim(), CONNECTION_GRANTED) && connectionListener != null)
+                        {
+                            connectionListener.onAccessGranted();
+                        }
+                        else if(Objects.equals(new String(response).trim(), CONNECTION_DENIED) && connectionListener != null)
+                        {
+                            connectionListener.onAccessDenied();
+                        }
+                    }
+                }
+                catch(IOException | JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-    private void sendBytes(byte[] myByteArray) throws IOException
+    private void sendBytes(Socket socket, byte[] myByteArray) throws IOException
     {
-        sendBytes(myByteArray, 0, myByteArray.length);
+        sendBytes(socket, myByteArray, 0, myByteArray.length);
     }
 
-    private void sendBytes(byte[] myByteArray, int start, int len) throws IOException
+    private void sendBytes(Socket socket, byte[] myByteArray, int start, int len) throws IOException
     {
         if(len < 0)
             throw new IllegalArgumentException("Negative length not allowed");
@@ -64,7 +105,7 @@ public class ServerCommunicationThread extends Thread implements Runnable
 
         // May be better to save the streams in the support class;
         // just like the socket variable.
-        OutputStream out = gameServerSocket.getOutputStream();
+        OutputStream out = socket.getOutputStream();
         DataOutputStream dos = new DataOutputStream(out);
 
         //dos.writeInt(len);
@@ -84,8 +125,15 @@ public class ServerCommunicationThread extends Thread implements Runnable
         return b;
     }
 
-    public interface ServerCommunication
+    public void setConnectionListener(ServerConnectionListener listener)
     {
-        void onReceive(byte[] response);
+        this.connectionListener = listener;
+    }
+
+    public interface ServerConnectionListener
+    {
+        void onAccessGranted();
+
+        void onAccessDenied();
     }
 }
